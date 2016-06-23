ef5db80dd90465de8cf42f05f4e73f6033723398
gguterman <gregoryguterman@gmail.com>
2016-06-23 16:04:40 -0700

    Add dependencies to package.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 var esprima = require("esprima");
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3001);
 
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
