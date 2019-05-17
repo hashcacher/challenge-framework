a0fa3d7efffb748310bc4353d503438dbd7b788d
Greg Guterman <gregoryguterman@gmail.com>
2019-05-16 22:12:15 -0700

    Fix up style imports

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
+import './styles/base.css';
+import './styles/khanacademy.css';
 import AppContainer from './App';
 import registerServiceWorker from './registerServiceWorker';
 
